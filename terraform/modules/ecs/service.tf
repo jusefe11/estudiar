7b433e8c664379961277910e4d8e690b33ac4fb0
Juan Sebastián Ferrer Bustos <jusefe11@hotmail.com>
2026-08-22 11:29:42 -0500

    amor

--- terraform/modules/ecs/service.tf
+++ terraform/modules/ecs/service.tf
@@ -39,3 +39,33 @@ resource "aws_ecs_service" "frontend" {
     aws_ecs_task_definition.frontend
   ]
 }
+
+resource "aws_ecs_service" "productcatalogservice" {
+
+  name            = "${var.project_name}-${var.environment}-productcatalogservice"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.productcatalogservice.arn
+
+  desired_count = 1
+
+  launch_type = "FARGATE"
+
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
+
+  network_configuration {
+
+    assign_public_ip = false
+
+    security_groups = [
+      var.ecs_security_group_id
+    ]
+
+    subnets = var.private_subnets
+
+  }
+
+  depends_on = [
+    aws_ecs_task_definition.productcatalogservice
+  ]
+}
